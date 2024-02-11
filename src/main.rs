ce05b0a576124ea5aeac45eccd63efa58998fb67
Colin Huber <cbhuber@gmail.com>
2024-02-11 09:41:34 -0700

    feat: Server struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Rust HTTP Server");
+    // let string = String::from("127.0.0.1:8080");
+    // let port = &string[10..];
+
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    fn run(self) {
+        println!("Listening on {}", self.addr)
+    }
 }

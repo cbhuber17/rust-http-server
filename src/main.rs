b31df2a76c94c289ca84ec55a25836ca02a06836
Colin Huber <cbhuber@gmail.com>
2024-02-11 10:15:12 -0700

    refactor: sub modules for server, method adn request

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,14 @@
+use http::Method;
+use http::Request;
+use server::Server;
+
+mod http;
+mod server;
+
 fn main() {
     // let string = String::from("127.0.0.1:8080");
     // let port = &string[10..];
 
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self { addr }
-    }
-
-    fn run(self) {
-        println!("Listening on {}", self.addr)
-    }
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
 }

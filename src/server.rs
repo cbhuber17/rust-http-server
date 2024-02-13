71bc3243a6812e3ec6fdcd2a59253c822a92aa74
Colin Huber <cbhuber@gmail.com>
2024-02-12 18:44:58 -0700

    feat: stream memory allocation.

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
-use ::std::net::TcpListener;
+use std::io::Read;
+use std::net::TcpListener;
 
 pub struct Server {
     addr: String,
@@ -12,6 +13,18 @@ impl Server {
     pub fn run(self) {
         println!("Listening on {}", self.addr);
 
-        let listener = TcpListener::bind(&self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buf = [0; 1024];
+                    stream.read(&mut buf);
+                }
+                Err(e) => {
+                    println!("Failed to establish a connection: {}", e)
+                }
+            }
+        }
     }
 }

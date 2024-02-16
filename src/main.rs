6d1d5f05e51a2ae9b2bac997eb0f1ff3ef2c372a
Colin Huber <cbhuber@gmail.com>
2024-02-16 13:58:23 -0700

    env: clean up code and suppress dead code warning

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-// use http::Method;
-// use http::Request;
+// #![allow(dead_code)]
+
 use server::Server;
 
 mod http;
 mod server;
 
 fn main() {
-    // let string = String::from("127.0.0.1:8080");
-    // let port = &string[10..];
-
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }

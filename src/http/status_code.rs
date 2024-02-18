4c8ae6b2515d41e8ef44f582d4eade4ebb3e9694
Colin Huber <cbhuber@gmail.com>
2024-02-18 09:16:33 -0700

    feat: status code and response

---
+++ src/http/status_code.rs
@@ -0,0 +1,26 @@
+use std::fmt::{write, Display, Formatter, Result as FmtResult};
+
+// This derive Copy/Clone allows to cast the enum as a uint in the fmt fn below
+// Debug allows to print the object to console
+#[derive(Clone, Copy, Debug)]
+pub enum StatusCode {
+    Ok = 200,
+    BadRequest = 400,
+    NotFound = 404,
+}
+
+impl StatusCode {
+    pub fn reason_phrase(&self) -> &str {
+        match self {
+            Self::Ok => "Ok",
+            Self::BadRequest => "Bad Request",
+            Self::NotFound => "Not Found",
+        }
+    }
+}
+
+impl Display for StatusCode {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", *self as u16)
+    }
+}
